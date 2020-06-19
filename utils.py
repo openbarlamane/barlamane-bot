3cffdb24965d32ed2963931412d1ca1d78364fbc
Nabil Elqatib <nabil.elqatib@hiboo.io>
2020-06-19 12:37:32 +0200

    Hide headers from page when taking screenshot

--- utils.py
+++ utils.py
@@ -21,20 +21,28 @@ def download_first_page_as_jpeg(dl_link, dest):
     os.remove(tmp)
     return dest
 
-# Take a screenshot of the content of 
-# a [written|oral] question
+# Take a screenshot of the content of a [written|oral] question
 def clip_question_verbatim_screenshot(url, dir='.'):
     options = Options()
     options.headless = True
-    d = webdriver.Firefox(options=options)
+
+    # use full path to geckodriver in order to avoid crontab PATH intricaties
+    d = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver", options=options)
     ob = Screenshot_Clipping.Screenshot()
     
     print("Clipping question text screenshot from: %s" % url)
     d.get(url)
+    
+    # delete page header (hinders remaining elements)
+    d.execute_script('document.getElementsByClassName("container")[1].remove()')
 
-    e = d.find_element_by_xpath('/html/body/section[5]/div/div/div[1]/div[1]/div[3]/div[2]/p')
+    try:
+        e = d.find_element_by_xpath('/html/body/section[5]/div/div/div[1]/div[1]/div[3]/div[2]/p')
+    except Exception as exc:
+        print("Exception: e", exc)
+        e = d.find_element_by_xpath('/html/body/section[5]/div/div/div[1]/div[1]/div[3]/div/p')
 
-    img_path = ob.get_element(d, e, dir)
+    img_path = ob.get_element(d, e, dir, to_hide=['class=mid-header', 'class=top-header'])
     print("Img path : %s" % img_path)
 
     d.quit()
