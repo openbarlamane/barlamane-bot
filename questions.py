ec12dd042c5b4affb997a6418596fe52910232c5
Nabil Elqatib <nabilelqatib@gmail.com>
2021-10-30 22:22:08 +0200

    Parallelize questions.py script (#4)
    Use multiprocessing, spawn system CPU cores processes to parse 2 pages
    at each iteration. At each iteration of the loop, the called function
    inserts the new questions and returns a list of the inserted items. If
    no items are inserted, the loop stops.

--- questions.py
+++ questions.py
@@ -7,15 +7,21 @@ Usage:
     $ python3.6 questions.py --written # for written questions
 """
 
+import logging
+from multiprocessing import Pool, current_process
 import sys
 import time
 import random
 from datetime import datetime
 
-from barlapy.parser import parse_questions_in_page
+# import barlapy.barlapy as bp
+from barlapy.barlapy.parser import parse_questions_in_page
 
 import twitter
-from utils import twitter_map, WRITTEN_Q_PAGE, ORAL_Q_PAGE, clip_question_verbatim_screenshot, connect_to_db
+from utils import *
+
+db = connect_to_db()
+questions_db = db["questions"]
 
 def match_twitter(author):
     if author in twitter_map.keys():
@@ -40,75 +46,59 @@ def format_tweet(qtype, row):
         snippet = t
     return h + a + " - " + snippet + " " + u
 
-def get_new_questions(questions_db, qtype):
-    page = 0
+def parse_page(i):
+    url = "%s?page=%s" % (ORAL_Q_PAGE, str(i))
+    logging.info("[PID:%d] Parsing page: %s" % (current_process().pid, url))
 
-    new_questions = []
-    found_in_db = False
-    while not found_in_db:
-        if qtype == 'written':
-            url = WRITTEN_Q_PAGE
-        elif qtype == 'oral':
-            url = ORAL_Q_PAGE
-        else:
-            print("Wrong parameter")
-            return []
+    ret = parse_questions_in_page(url)
 
-        url += "?page=%s" % str(page)
+    inserted = []
 
-        print("Fetching questions in page: %s" % url)
-        questions = parse_questions_in_page(url)
-        for q in questions:
-            count = questions_db.count_documents({"id": int(q.get_id()), "type": qtype})
-            if count == 1:
-                print("Found question %d in db" % q.get_id())
-                found_in_db = True
-                break
-            elif count > 1:
-                print("Question %d has %d duplicates!" % (q.get_id(), count))
-                return []
-            else:
-                print("Question %d not in db" % q.get_id())
+    for q in ret:
+        count = questions_db.count_documents({"id": int(q.get_id()), "type": "oral"})
+        if count == 0:
+            res = questions.insert_one(d)
+            logger.debug("Inserted new question, _id: %s" % res.inserted_id)
+            inserted.append(q)
 
-            new_questions.append(q)
+            t = format_tweet(qtype, {'author': q.authors, 'topic': q.topic, 'url': q.get_url()})
+            # TODO : FIXME selenium geckodriver does not work, is this still true?
+            question_text = clip_question_verbatim_screenshot(q.get_url(), './written_questions_text')
+            twitter.tweet(t, True, question_text)
+            time.sleep(random.randint(2, 30))
 
-        page += 1
-
-    return new_questions
+    return inserted
+    
 
 def main(qtype):
-    db = connect_to_db()
-    questions = db["questions"]
+    pool = Pool()
 
-    new_questions = get_new_questions(questions, qtype)
+    i = 0
+    step = 2
+    keepgoing = True
+    while keepgoing:
+        logging.debug("Parsing from %d.." % i)
+        ret = pool.map(parse_page, range(i, i + step)) # parse 2 pages at each iteration
 
-    if len(new_questions) == 0:
-        print('No new questions')
-        return
+        keepgoing = False
+        for elt in ret:
+            if len(elt) > 0:
+                keepgoing = True
 
-    print('There are %d new questions' % len(new_questions))
-    for q in new_questions:
-
-        d = q.to_dict()
-        d['updated_at'] = datetime.now()
-        res = questions.insert_one(d)
-
-        print("Inserted new question, _id: %s" % res.inserted_id)
-
-        t = format_tweet(qtype, {'author': q.authors, 'topic': q.topic, 'url': q.get_url()})
-        # TODO : FIXME selenium geckodriver does not work
-        # question_text = clip_question_verbatim_screenshot(q.get_url(), './written_questions_text')
-        # twitter.tweet(t, True, question_text)
-        twitter.tweet(t, False)
-        time.sleep(random.randint(2, 30))
+        i += step
+    logging.debug("Done")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Argument needed (--oral/-o, --written/-w)")
+        logging.error("Argument needed (--oral/-o, --written/-w)")
         sys.exit()
 
-    print(datetime.now().strftime("%d/%m/%Y %H:%M"))
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.basicConfig(filename='questions.log', level=logging.DEBUG, format='%(asctime)s %(levelname)7s %(message)s')
+
     if sys.argv[1] == '-w' or sys.argv[1] == "--written":
+        logging.info("Parsing written questions")
         main("written")
     elif sys.argv[1] == '-o' or sys.argv[1] == "--oral":
+        logging.info("Parsing oral questions")
         main("oral")
